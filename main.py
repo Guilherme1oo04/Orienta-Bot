fd22f08354a92606debe73c79811f1f0e1d1f889
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-08-30 14:49:46 -0300

    Testando funcionalidade de botões

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import os
 import telebot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 from dotenv import load_dotenv
 
 load_dotenv()
 
 chave_api = os.environ["API_TOKEN"]
 
-estagioBot = telebot.Telebot(token=chave_api)
+estagioBot = telebot.TeleBot(token=chave_api)
+
+#Teclado inicial
+keyboardInicio = ReplyKeyboardMarkup(resize_keyboard=True)
+
+#Botão sobre
+button_sobre = KeyboardButton('/sobre')
+#Botão dúvidas
+button_duvidas = KeyboardButton('/duvidas')
+
+keyboardInicio.add(button_sobre, button_duvidas)
+
+@estagioBot.message_handler(commands=['start'])
+def handle_start(message):
+    texto = "Olá, seja bem vindo! \nEsse é o Orienta Bot. para prosseguir, escolha uma das opções"
+    estagioBot.reply_to(message, texto, reply_markup=keyboardInicio)
 
 estagioBot.polling()
