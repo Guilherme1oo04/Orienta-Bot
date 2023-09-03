f3d3f02cd18410f39ce9da7c442a4b41ae7a4bc4
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2023-09-03 11:27:46 -0300

    add InlineKeyboardButtons

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import telebot
-from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,25 +10,38 @@ chave_api = os.environ["API_TOKEN"]
 estagioBot = telebot.TeleBot(token=chave_api)
 
 #Teclado Start
-keyboardStart = ReplyKeyboardMarkup(resize_keyboard=True)
-button_start = KeyboardButton("/start")
+keyboardStart = InlineKeyboardMarkup()
+button_start = InlineKeyboardButton("Start", callback_data="start")
 keyboardStart.add(button_start)
 
 #Teclado Sim / Não
-keyboardSN = ReplyKeyboardMarkup(resize_keyboard=True)
-buttonSim = KeyboardButton("Sim")
-buttonNao = KeyboardButton("Não")
+keyboardSN = InlineKeyboardMarkup()
+buttonSim = InlineKeyboardButton("Sim", callback_data="Sim")
+buttonNao = InlineKeyboardButton("Não", callback_data="Nao")
 keyboardSN.add(buttonSim, buttonNao)
+
+@estagioBot.callback_query_handler(func= lambda call: call.data == "Sim" or call.data == "Nao")
+def responseSN(callback):
+    if (callback.data == "Sim"):
+        command_duvidas(callback.message)
+
+    elif (callback.data == "Nao"):
+        estagioBot.reply_to(callback.message, "Obrigado por utilizar, Nos ajude dando um feedback")
+
 
 #Resposta padrão do bot caso não seja um comando
 @estagioBot.message_handler(func=lambda message: not message.text.startswith('/'))
 def handle_text(message):
-    estagioBot.reply_to(message, 'Para utilizar o bot, comece com o botão /start abaixo', reply_markup=keyboardStart)
+    estagioBot.reply_to(message, 'Para utilizar o bot, comece com o botão start abaixo', reply_markup=keyboardStart)
+
+@estagioBot.callback_query_handler(func= lambda call: call.data == "start")
+def callback_start(callback):
+    handle_start(callback.message)
 
 #Teclado inicial
-keyboardInicio = ReplyKeyboardMarkup(resize_keyboard=True)
-button_sobre = KeyboardButton('/sobre')
-button_duvidas = KeyboardButton('/duvidas')
+keyboardInicio = InlineKeyboardMarkup()
+button_sobre = InlineKeyboardButton('Sobre', callback_data="sobre")
+button_duvidas = InlineKeyboardButton('Duvidas', callback_data="duvidas")
 keyboardInicio.add(button_sobre, button_duvidas)
 
 @estagioBot.message_handler(commands=['start'])
@@ -41,17 +54,30 @@ def handle_start(message):
 def command_sobre(message):
     estagioBot.reply_to(message, "Explicação do projeto")
     estagioBot.reply_to(message, "Deseja ver alguma das dúvidas respondidas?", reply_markup=keyboardSN)
-    estagioBot.register_next_step_handler(message, responseSN)
 
-def responseSN(message):
-    if (message.text == "Sim"):
-        command_duvidas(message)
+@estagioBot.callback_query_handler(func= lambda call: call.data == "sobre")
+def callback_sobre(callback):
+    command_sobre(callback.message)
 
-    elif (message.text == "Não"):
-        estagioBot.reply_to(message, "Obrigado por utilizar, Nos ajude dando um feedback")
+
+keyboardDuvidas = InlineKeyboardMarkup()
+empresa = InlineKeyboardButton("Empresa", callback_data="empresa")
+checkEstagiario = InlineKeyboardButton("Checklist Estagiário", callback_data="checklistEstagio")
+sice = InlineKeyboardButton("Sice", callback_data="sice")
+dicasRelatorio = InlineKeyboardButton("Dicas Relatorio", callback_data="dicasRelatorio")
+mediacao = InlineKeyboardButton("Mediação", callback_data= "mediacao")
+bolsaEstagio = InlineKeyboardButton("Bolsa Estagio", callback_data="bolsaEstagio")
+keyboardDuvidas.row(empresa, checkEstagiario).row(sice, dicasRelatorio).row(mediacao, bolsaEstagio)
 
 @estagioBot.message_handler(commands=["duvidas"])
 def command_duvidas(message):
-    estagioBot.reply_to(message, "Duvidas")
+    
+    texto = "Essas são as dúvidas respondidas, escolha alguma para poder ver as respostas"
+    estagioBot.reply_to(message, texto, reply_markup=keyboardDuvidas)
+
+@estagioBot.callback_query_handler(func= lambda call: call.data == "duvidas")
+def callback_duvidas(callback):
+    command_duvidas(callback.message)
+        
 
 estagioBot.polling()
